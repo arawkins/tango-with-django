1fac1ce6e2d055840b9e5f175ef38ff8c25c1abf
Alan Rawkins <arawkins@gmail.com>
2016-06-30 12:35:52 -0700

    Added basic static files, templates

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Rango</h1><ul><li><a href='/rango/'>Home</a></li><li><a href='/rango/about/'>About</a></li><p>Home Page</p>")
+    context_dict = {'content': 'Welcome to Rango!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("<h1>Rango</h1><ul><li><a href='/rango/'>Home</a></li><li><a href='/rango/about/'>About</a></li><p>About Page</p>")
+    context_dict = {'content': 'This page was made by Alan'}
+    return render(request, 'rango/index.html', context=context_dict)
