82eba35443b86dcd3dfe8d4da5bbad43514df299
Alan Rawkins <arawkins@gmail.com>
2016-06-30 11:28:13 -0700

    Added about page with basic html nagivation

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello")
+    return HttpResponse("<h1>Rango</h1><ul><li><a href='/rango/'>Home</a></li><li><a href='/rango/about/'>About</a></li><p>Home Page</p>")
+
+def about(request):
+    return HttpResponse("<h1>Rango</h1><ul><li><a href='/rango/'>Home</a></li><li><a href='/rango/about/'>About</a></li><p>About Page</p>")
