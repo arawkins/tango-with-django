e9a517ca6ddf7850dfea158300ecda6d4e22c4e2
Alan Rawkins <arawkins@gmail.com>
2016-06-30 15:24:47 -0700

    Added slug field, updated index page with category listings, updated admin

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-    url(r'^about/', views.about, name='rango-about'),
-    url(r'^$', views.index, name='rango-index'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^$', views.index, name='index'),
 ]
